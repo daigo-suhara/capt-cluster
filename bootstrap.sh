44f2784a47ccd433a38a70ebfd8bd313cc713c7c
Daigo Suhara <suharadaigo@gmail.com>
2026-05-10 01:46:06 +0900

    Add minimal Minikube bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eo pipefail
+
+if ! command -v minikube &> /dev/null; then
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube
+    rm minikube-linux-amd64
+fi
+
+minikube start --driver=docker --memory=8192 --cpus=4
+
+if ! command -v kubectl &> /dev/null; then
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    rm kubectl
+fi
+
+kubectl create namespace argocd || true
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl wait --for=condition=available deployment/argocd-server -n argocd --timeout=300s
+
+kubectl apply -f https://raw.githubusercontent.com/daigo-suhara/capt-cluster/master/argocd/app-of-apps.yaml

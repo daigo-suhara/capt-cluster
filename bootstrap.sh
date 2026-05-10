70d4ab92023462f134b3e4cfdd3dfe7f2452f76a
Daigo Suhara <suharadaigo@gmail.com>
2026-05-10 15:17:50 +0900

    Use containerd runtime for minikube none driver

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ if ! command -v minikube &> /dev/null; then
     rm minikube-linux-amd64
 fi
 
-sudo minikube start --driver=none
+sudo minikube start --driver=none --container-runtime=containerd
 
 if ! command -v kubectl &> /dev/null; then
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
